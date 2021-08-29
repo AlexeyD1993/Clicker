2fe1827ef55ee9d463d9713cbc0cf5accc492e43
Alexey <alexeyD1993@yandex.ru>
2021-08-29 14:50:02 +0300

    Добавьте файлы проекта.

---
+++ Clicker/src/Params/SeleniumParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicker.src.Params
+{
+    class SeleniumParams
+    {
+        private string browser;
+        private string finderUrl;
+        private string proxyIp;
+        private string proxyPort;
+        private string findUrl;
+
+    }
+}
---
+++ Clicker/src/Selenium/SeleniumWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicker.src.Selenium
+{
+    class SeleniumWorker
+    {
+        private Selenium
+        public SeleniumWorker(Params.SeleniumParams seleniumParams)
+        {
+            
+        }
+    }
+}
