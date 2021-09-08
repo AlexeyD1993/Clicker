2984c3d6890c58628671874540118ea57ce0b438
Alexey Dudin <dudin.lesha@gmail.com>
2021-09-08 16:48:19 +0300

    - fix logger in project

---
+++ Logger/ImageLogger.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicker.src.Logger
+{
+    class ImageLogger
+    {
+        private string imagePath= "";
+
+        public ImageLogger(string imagePath)
+        {
+            this.imagePath = imagePath;
+            if (Directory.Exists(imagePath))
+                throw new Exception(string.Format("Директория {0} уже существует", imagePath));
+            else
+                Directory.CreateDirectory(imagePath);
+        }
+
+        public void Add(IWebDriver webDriver, string paramName)
+        {
+            string fileName = string.Format("{0}-{1}-{2}-{3}_{4}_{5}-{6}.png", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second, paramName);
+            Screenshot ss = ((ITakesScreenshot)webDriver).GetScreenshot();
+            ss.SaveAsFile(Path.Combine(imagePath, fileName), ScreenshotImageFormat.Png);
+        }
+    }
+}
---
+++ Logger/TextLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicker.src.Logger
+{
+    class TextLogger
+    {
+        private string logFileName = "";
+        public TextLogger(string logFileName)
+        {
+            this.logFileName = logFileName;
+            if (File.Exists(logFileName))
+                throw new Exception(string.Format("Ошибка создания текстового протокола. Протокол с именем {0} уже существует", logFileName));
+            else
+                File.CreateText(logFileName);
+        }
+
+        public void Add(string message)
+        {
+            string dateTime = string.Format("{0}-{1}-{2}-{3}:{4}:{5}", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+            File.AppendAllText(logFileName, string.Format("{0}: {1}", dateTime, message));
+        }
+    }
+}
